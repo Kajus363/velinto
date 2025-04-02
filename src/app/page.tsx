6a7755c89b6cc6dccb3da51bc2c01728b234bad5
Kajus363 <Kajuskuzminas@gmail.com>
2025-04-02 19:49:47 +0300

    +1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@ import FramerComponent from "@/components/FramerComponent";
 export default function Home() {
     return (
         <div>
-            <h1 style={{ color: "white" }}>Home Page</h1>
             <FramerComponent />
         </div>
     );
